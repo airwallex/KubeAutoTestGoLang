eaa570e7141c5c01ca2d3f26b59a34c05e6c54ff
James Griffis <james.griffis@airwallex.com>
2017-06-13 16:36:38 +1000

    Testing Failure Case

--- main_test.go
+++ main_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestMessage(t *testing.T) {
-	funResponse := string("Hello World")
+	funResponse := string("BANANA")
 
 	if !strings.Contains(funResponse, Message()) {
 		t.Errorf("Expecting %v but got %v", funResponse, Message())
